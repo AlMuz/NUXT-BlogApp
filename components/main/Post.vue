bdaa971aeb21caf7042a6bab3b7b20b26fd911de
AlMuz <almuz@inbox.lv>
2020-01-06 23:48:21 +0200

    Post component styling

---
+++ components/main/Post.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-card
+    shadow="hover"
+    :body-style="{padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+      <h3>Post title</h3>
+      <small>
+        <i class="el-icon-time" />
+        {{ new Date().toLocaleString() }}
+      </small>
+    </header>
+    <div class="post-body">
+      <img src="https://sun9-42.userapi.com/c857424/v857424648/14ccac/em9uWu5h5Ac.jpg" alt="Post Image" class="post-image">
+    </div>
+    <footer class="post-footer">
+      <el-button @click="openPost" round>
+        Open
+      </el-button>
+      <span>
+        <i class="el-icon-message"/>
+        12
+      </span>
+    </footer>
+  </el-card>
+</template>
+
+<script>
+export default {
+  methods: {
+    openPost () {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .post {
+    margin-bottom: 1.5rem;
+  }
+  .post-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .post-image {
+    width: 100%;
+    height: auto;
+  }
+  .post-body {
+  }
+  .post-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-
-  </div>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="24" :md="12" :lg="10">
+      <AppPost
+      v-for="post in 3"
+      :key="post"
+      />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
+import AppPost from '@/components/main/Post'
 export default {
+  components: {
+    AppPost
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
