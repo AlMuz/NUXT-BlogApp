9729fc71a4fe2720ce0d9cb96fead19ab236852f
AlMuz <almuz@inbox.lv>
2020-01-27 15:50:58 +0200

    Form validation

--- components/main/CommentForm.vue
+++ components/main/CommentForm.vue
@@ -21,6 +21,7 @@
     </el-form-item>
     <el-form-item>
       <el-button
+        :loading="loading"
         type="primary"
         native-type="submit">
         Add comment
@@ -33,6 +34,7 @@
 export default {
   data () {
     return {
+      loading: false,
       controls: {
         name: '',
         text: ''
@@ -51,7 +53,22 @@ export default {
     onSubmit () {
       this.$refs.form.validate((valid) => {
         if (valid) {
-          console.log('valid')
+          this.loading = true
+
+          const formData = {
+            name: this.controls.name,
+            text: this.controls.text,
+            postId: ''
+          }
+          console.log(formData)
+          try {
+            setTimeout(() => {
+              this.$emit('created')
+              this.$message.success('Comment successfully added')
+            }, 2000)
+          } catch (e) {
+            this.loading = false
+          }
         }
       })
     }
--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -25,7 +25,10 @@
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </main>
     <footer>
-      <CommentForm />
+      <CommentForm
+        v-if="canAddComment"
+        @created="createCommentHandler"
+      />
 
       <div class="comments" v-if="true">
         <Comment
@@ -48,6 +51,16 @@ import CommentForm from '@/components/main/CommentForm'
 export default {
   validate ({ params }) {
     return Boolean(params.id)
+  },
+  data () {
+    return {
+      canAddComment: true
+    }
+  },
+  methods: {
+    createCommentHandler () {
+      this.canAddComment = false
+    }
   },
   components: {
     Comment,
