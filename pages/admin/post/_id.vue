b3eccb8de18f3b212fa5445e0c32ccf0a5818705
AlMuz <almuz@inbox.lv>
2020-04-12 20:43:03 +0300

    post editing

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -1,16 +1,105 @@
 <template>
-  <div>
-    <el-breadcrumb separator="/">
+  <div class="page-wrap">
+    <el-breadcrumb separator="/" class="mb">
       <el-breadcrumb-item :to="{ path: '/admin/list' }">
         Posts
       </el-breadcrumb-item>
-      <el-breadcrumb-item> Post - {{ $route.params.id }} </el-breadcrumb-item>
+      <el-breadcrumb-item> Post - {{ post.title }} </el-breadcrumb-item>
     </el-breadcrumb>
+    <el-form
+      ref="form"
+      :model="controls"
+      :rules="rules"
+      label-width="120px"
+      label-position="top"
+      @submit.native.prevent="onSubmit"
+    >
+      <!-- <h3>Login to administrator</h3>  -->
+      <el-form-item label="Text in format .md or .html" prop="text">
+        <el-input
+          v-model.trim="controls.text"
+          type="textarea"
+          resize="none"
+          :rows="10"
+        />
+      </el-form-item>
+
+      <div class="mb">
+        <small>
+          <i class="el-icon-time" />
+          <span>
+            {{ new Date(post.date).toLocaleString() }}
+          </span>
+        </small>
+      </div>
+
+      <el-form-item>
+        <el-button :loading="loading" type="primary" native-type="submit">
+          Update
+        </el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'admin'
+  layout: 'admin',
+  async asyncData({ store, params }) {
+    const post = await store.dispatch('post/fetchPostById', params.id)
+    console.log(post)
+
+    return { post }
+  },
+  data() {
+    return {
+      loading: false,
+      controls: {
+        text: ''
+      },
+      rules: {
+        text: [
+          { required: true, message: 'text cant be empty', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.form.validate(async (valid) => {
+        if (valid) {
+          this.loading = true
+
+          const formData = {
+            text: this.controls.text,
+            id: this.post._id
+          }
+
+          try {
+            await this.$store.dispatch('post/updatePost', formData)
+            this.$message.success('Post updated')
+            this.loading = false
+          } catch (error) {
+            this.$message.error('Post not updated')
+            this.loading = false
+          }
+        }
+      })
+    }
+  },
+  validate({ params }) {
+    return Boolean(params.id)
+  },
+  head() {
+    return {
+      title: `Post | ${this.post.title}`
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.page-wrap {
+  width: 600px;
+}
+</style>
--- store/post.js
+++ store/post.js
@@ -1,35 +1,48 @@
+const posts = [
+  {
+    _id: Math.floor(Math.random() * Math.floor(100)),
+    title: 'Post',
+    date: new Date(),
+    views: 22,
+    comments: [1, 2]
+  },
+  {
+    _id: Math.floor(Math.random() * Math.floor(100)),
+    title: 'Post2',
+    date: new Date(),
+    views: 223,
+    comments: [1, 4]
+  },
+  {
+    _id: Math.floor(Math.random() * Math.floor(100)),
+    title: 'Post7',
+    date: new Date(),
+    views: 22,
+    comments: [2, 2]
+  }
+]
+
 /* eslint-disable no-empty-pattern */
 export const actions = {
   async fetchAdminPosts({}) {
     return await new Promise((resolve) => {
       setTimeout(() => {
-        resolve([
-          {
-            _id: Math.floor(Math.random() * Math.floor(100)),
-            title: 'Post',
-            date: new Date(),
-            views: 22,
-            comments: [1, 2]
-          },
-          {
-            _id: Math.floor(Math.random() * Math.floor(100)),
-            title: 'Post2',
-            date: new Date(),
-            views: 223,
-            comments: [1, 4]
-          },
-          {
-            _id: Math.floor(Math.random() * Math.floor(100)),
-            title: 'Post7',
-            date: new Date(),
-            views: 22,
-            comments: [2, 2]
-          }
-        ])
-      })
+        resolve(posts)
+      }, 1000)
+    })
+  },
+  async fetchPostById({}, id) {
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(posts.find((post) => (post._id = id)))
+      }, 1000)
     })
   },
   async deletePost({}, id) {
     console.log(id)
+  },
+  async updatePost({}, { id, text }) {
+    console.log(id)
+    console.log(text)
   }
 }
