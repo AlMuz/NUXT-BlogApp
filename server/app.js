12bc79058108e94315897c7c799189506732e33d
AlMuz <almuz@inbox.lv>
2020-04-14 16:05:42 +0300

    Mongoose installed and works with external DB

--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const keys = require('./keys')
 const app = express()
+
+mongoose
+  .connect(keys.MONGO_URI, { dbName: 'blog' })
+  .then(() => console.log('MONGO connected'))
+  .catch((error) => console.log(error))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
--- server/keys/keys.dev.js
+++ server/keys/keys.dev.js
@@ -1,1 +1,3 @@
-module.exports = {}
+module.exports = {
+  MONGO_URI: `mongodb+srv://dbuser:jOBoxzU86vPz3Av2@nuxt-blogapp-patr1.mongodb.net/`
+}
