827baf77fb4093c8a9681dde6fe1d2c10b1a98cc
AlMuz <almuz@inbox.lv>
2020-04-15 18:07:09 +0300

    Passport strategy added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
 
 const authRoutes = require('./routes/auth.routes')
+const passportStrategy = require('./middleware/passport-strategy')
 const keys = require('./keys')
 const app = express()
 
@@ -10,6 +12,9 @@ mongoose
   .connect(keys.MONGO_URI, { dbName: 'blog' })
   .then(() => console.log('MONGO connected'))
   .catch((error) => console.log(error))
+
+app.use(passport.initialize())
+passport.use(passportStrategy)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
