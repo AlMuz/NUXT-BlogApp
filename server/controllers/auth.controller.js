bad5f1f129e859747cb106505bf9713cc80c8fd0
AlMuz <almuz@inbox.lv>
2020-04-15 15:49:12 +0300

    administrator login

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,3 +1,31 @@
-module.exports.login = (req, res) => {}
+const bcrypt = require('bcrypt-nodejs')
+const jwt = require('jsonwebtoken')
+const keys = require('../keys')
+const User = require('../models/user.model')
+
+module.exports.login = async (req, res) => {
+  const candidate = await User.findOne({ username: req.body.username })
+  if (candidate) {
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      candidate.password
+    )
+    if (isPasswordCorrect) {
+      const token = jwt.sign(
+        {
+          login: candidate.login,
+          userId: candidate._id
+        },
+        keys.JWT,
+        { expiresIn: 60 * 60 }
+      )
+      res.json({ token })
+    } else {
+      res.status(401).json({ message: 'Password not correct' })
+    }
+  } else {
+    res.status(404).json({ message: 'User not found' })
+  }
+}
 
 module.exports.createUser = (req, res) => {}
