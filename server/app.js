09a703aec6fcc6ab7f733ad96458de4769348b6b
AlMuz <almuz@inbox.lv>
2020-04-17 14:54:27 +0300

    Comments functionality added

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const passport = require('passport')
 
 const authRoutes = require('./routes/auth.routes')
 const postRoutes = require('./routes/post.routes')
+const commentRoutes = require('./routes/comment.routes')
 const passportStrategy = require('./middleware/passport-strategy')
 const keys = require('./keys')
 const app = express()
@@ -22,5 +23,6 @@ app.use(bodyParser.json())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
+app.use('/api/comment', commentRoutes)
 
 module.exports = app
