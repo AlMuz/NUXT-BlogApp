91bc5ff81a85d9c4fc5921da738b95c672774530
AlMuz <almuz@inbox.lv>
2020-04-22 14:36:38 +0300

    Persist jwt token

--- store/auth.js
+++ store/auth.js
@@ -1,3 +1,9 @@
+import jwtDecode from 'jwt-decode'
+// parsing cookies
+import Cookie from 'cookie'
+// write and delete cookies
+import Cookies from 'js-cookie'
+
 export const state = () => ({
   token: null
 })
@@ -18,8 +24,6 @@ export const actions = {
         '/api/auth/admin/login',
         formData
       )
-      console.log(token)
-
       dispatch('setToken', token)
     } catch (e) {
       commit('setError', e, { root: true })
@@ -33,17 +37,43 @@ export const actions = {
       commit('setError', e, { root: true })
     }
   },
-  setToken({ commit }, token) {
+  async setToken({ commit }, token) {
     this.$axios.setToken(token, 'Bearer')
     commit('setToken', token)
+    await Cookies.set('jwt-token', token)
+  },
+  autoLogin({ dispatch }) {
+    const cookieStr = process.browser
+      ? document.cookie
+      : this.app.context.req.headers.cookie
+    const cookies = Cookie.parse(cookieStr || '') || {}
+    const token = cookies['jwt-token']
+
+    if (isJWTValid(token)) {
+      dispatch('setToken', token)
+    } else {
+      dispatch('logout')
+    }
   },
   logout({ commit }) {
     this.$axios.setToken(false)
     commit('clearToken')
+    Cookies.remove('jwt-token')
   }
 }
 
 export const getters = {
   isAuthenticated: (state) => Boolean(state.token),
   token: (state) => state.token
+}
+
+function isJWTValid(token) {
+  if (!token) {
+    return false
+  }
+
+  const jwtData = jwtDecode(token) || {}
+  const expires = jwtData.exp || 0
+
+  return new Date().getTime() / 1000 < expires
 }
